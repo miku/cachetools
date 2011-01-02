6bcd7f954091eccd7b80656b857ed8b9f5cd3c34
miku <martin.czygan@gmail.com>
2011-01-02 16:12:12 +0100

    stubs

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='cachetools',
+    version='0.0.1',
+    packages=['cachetools',],
+    license='TBA',
+    long_description=open('README.txt').read(),
+)
