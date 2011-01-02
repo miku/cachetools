586cf3938eeb8a5fe4f0d03617f2846bf18bbf47
miku <martin.czygan@gmail.com>
2011-01-02 20:07:50 +0100

    drop .txt

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ import setuptools
 
 setup(
     name='cachetools',
+    author='Martin Czygan',
+    author_email='martin.czygan@gmail.com',
+    url='https://github.com/miku/cachetools',
     version='0.0.1',
     packages=['cachetools',],
     license='TBA',
-    long_description=open('README.txt').read(),
+    long_description=open('README').read(),
 )
